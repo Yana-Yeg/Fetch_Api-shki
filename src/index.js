edcc2595210cac46b60a15d20fd0137afc4a6eea
Irina Sabaliauskiene <sabaliauskieneirina@gmail.com>
2022-01-21 14:37:18 +0100

    ready select countries

--- src/index.js
+++ src/index.js
@@ -2,11 +2,38 @@ import './sass/main.scss';
 import fetchApiGet from './fetchApiGet';
 import card from './templates/card.hbs';
 import smallCard from './templates/smallCard.hbs';
+import code from './countries.json';
 
-fetchApiGet('some', 'US').then(events => {
+
+const refs = {
+    form: document.querySelector('form'),
+    select: document.querySelector('.form-select'),
+    smallCards:document.querySelector('.main__grid-small-cards')
+  };
+ 
+  
+//   refs.form.addEventListener('change', searchEvents);
+
+  console.log(code)
+const markup = code.map(el=>`<option value="${el.code}">${el.name}</option>`).join("")
+console.log(refs.select)
+
+refs.select.insertAdjacentHTML("beforeend",markup);
+
+  
+//   async function searchEventst(event) {
+//     event.preventDefault();
+//     page = 1;
+//     const {
+//       elements: { searchQuery },
+//     } = event.currentTarget;
+    // searchAnimal = searchQuery.value.trim();
+// } 
+
+fetchApiGet('eagles', 'US').then(events => {
     console.log(events);
     const markup = smallCard(events);
-    document.body.insertAdjacentHTML('beforeend', markup);
+    refs.smallCards.insertAdjacentHTML('beforeend', markup);
 } );
 
 // fetchApiGet('some', 'US').then(events => {
@@ -15,5 +42,7 @@ fetchApiGet('some', 'US').then(events => {
     
 //     document.body.insertAdjacentHTML('beforeend', markup);
 // } );
+
+
 
 
