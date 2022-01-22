c6ff43d067b65bced672daf827dda5e79e0f3d91
Lenko Yevhen <userlekko@gmail.com>
2022-01-22 15:29:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -71,48 +71,48 @@ function searchEvents(event) {
 
 
 //переделка нового объпкта и по клику вызов карточки для модалки
-// refs.mainList.addEventListener('click', onClick);
-// function onClick(e) {
-//     // e.preventDefault();
-//     if (e.target.nodeName !== 'IMG') return;
-//     console.log(e.target.dataset.id);
-//     const id = e.target.dataset.id;
-//     fetchApiById(id).then(data => {
-//         showModal(...data)
-//          refs.closeModalBtn.addEventListener('click', e => {
-//             refs.modal.classList.toggle('is-hidden');
-//   });
-//         const newEvents = data.map(event => {
-//             return {
-//                 id: event.id,
-//                 url: event.url,
-//                 name: event.name,
-//                 info: event.info,
-//                 localDate: event.dates.start.localDate,
-//                 localTime: event.dates.start.localTime,
-//                 timezone: event.dates.timezone,
-//                 priceRanges: event.priceRanges,
-//                 placeName: event._embedded.venues[0].name,
-//                 cityName: event._embedded.venues[0].city.name,
-//                 countryName: event._embedded.venues[0].country.name,
-//                 image: event.images.find(img => {
-//                     if (document.body.offsetWidth <= 480) {
-//                         return img.url.includes("ARTIST_PAGE")
-//                     }
-//                     if (document.body.offsetWidth < 1280 && document.body.offsetWidth > 480) {
-//                         return img.url.includes("RETINA_PORTRAIT_16_9")
-//                     }
-//                     if (document.body.offsetWidth >= 1280) {
-//                         return img.url.includes("TABLET_LANDSCAPE_3_2")
-//                     }
-//                 }),
-//             };
-//         });
-//         // console.log('new',newEvents);
-//         // const markup = card(newEvents);
-//         // refs.mainList.innerHTML = markup;
+refs.mainList.addEventListener('click', onClick);
+function onClick(e) {
+    // e.preventDefault();
+    if (e.target.nodeName !== 'IMG') return;
+    console.log(e.target.dataset.id);
+    const id = e.target.dataset.id;
+    fetchApiById(id).then(data => {
+        showModal(...data)
+         refs.closeModalBtn.addEventListener('click', e => {
+            refs.modal.classList.toggle('is-hidden');
+  });
+        const newEvents = data.map(event => {
+            return {
+                id: event.id,
+                url: event.url,
+                name: event.name,
+                info: event.info,
+                localDate: event.dates.start.localDate,
+                localTime: event.dates.start.localTime,
+                timezone: event.dates.timezone,
+                priceRanges: event.priceRanges,
+                placeName: event._embedded.venues[0].name,
+                cityName: event._embedded.venues[0].city.name,
+                countryName: event._embedded.venues[0].country.name,
+                image: event.images.find(img => {
+                    if (document.body.offsetWidth <= 480) {
+                        return img.url.includes("ARTIST_PAGE")
+                    }
+                    if (document.body.offsetWidth < 1280 && document.body.offsetWidth > 480) {
+                        return img.url.includes("RETINA_PORTRAIT_16_9")
+                    }
+                    if (document.body.offsetWidth >= 1280) {
+                        return img.url.includes("TABLET_LANDSCAPE_3_2")
+                    }
+                }),
+            };
+        });
+        // console.log('new',newEvents);
+        // const markup = card(newEvents);
+        // refs.mainList.innerHTML = markup;
         
         
-//     })
-// }
+    })
+}
 
