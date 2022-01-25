debbcacf911068cf13341fbcfbf0150f1786d1ee
pylypchukroman <romanpylypchuk@gmail.com>
2022-01-25 19:40:14 +0200

    додає логіку скролла вверх сторінки

--- src/skroll-up.js
+++ src/skroll-up.js
@@ -1,16 +1,42 @@
-$(document).ready(function () {
-     $('#raketa-js').fadeOut();
-            $(window).scroll(function() {
-                if ($(this).scrollTop() > 300) {
-                    $('#raketa-js').fadeIn();
-                } else {
-                    $('#raketa-js').fadeOut();
-                }
-            });
-$("#raketa-js").on("click","a", function (event) {
-        event.preventDefault();
-        const id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1300);
+
+function scrollTo(to, duration = 700) {
+    const
+        element = document.scrollingElement || document.documentElement,
+        start = element.scrollTop,
+        change = to - start,
+        startDate = +new Date(),
+        easeInOutQuad = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        },
+        animateScroll = function () {
+            const currentDate = +new Date();
+            const currentTime = currentDate - startDate;
+            element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+            if (currentTime < duration) {
+                requestAnimationFrame(animateScroll);
+            }
+            else {
+                element.scrollTop = to;
+            }
+        };
+    animateScroll();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    let btn = document.querySelector('.raketa');
+    window.addEventListener('scroll', function () {
+        if (pageYOffset > 300) {
+            btn.classList.remove('visually-hidden');
+
+        } else {
+            btn.classList.add('visually-hidden');
+        }
     });
-        });
+    btn.onclick = function (click) {
+        click.preventDefault();
+        scrollTo(0, 1000);
+    }
+});
